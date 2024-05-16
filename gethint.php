f10b8954fba2e48cb6b31f6b86cf9b17c039e78b
salasvictor22 <jsalasag@unsa.edu.pe>
2024-05-15 22:59:48 -0500

    subiendo los archivos de los ejercicios 4,5,6 y 7

---
+++ gethint.php
@@ -0,0 +1,56 @@
+<?php
+// Array con nombres
+$a[] = "Anna";
+$a[] = "Brittany";
+$a[] = "Cinderella";
+$a[] = "Diana";
+$a[] = "Eva";
+$a[] = "Fiona";
+$a[] = "Gunda";
+$a[] = "Hege";
+$a[] = "Inga";
+$a[] = "Johanna";
+$a[] = "Kitty";
+$a[] = "Linda";
+$a[] = "Nina";
+$a[] = "Ophelia";
+$a[] = "Petunia";
+$a[] = "Amanda";
+$a[] = "Raquel";
+$a[] = "Cindy";
+$a[] = "Doris";
+$a[] = "Eve";
+$a[] = "Evita";
+$a[] = "Sunniva";
+$a[] = "Tove";
+$a[] = "Unni";
+$a[] = "Violet";
+$a[] = "Liza";
+$a[] = "Elizabeth";
+$a[] = "Ellen";
+$a[] = "Wenche";
+$a[] = "Vicky";
+
+// Obtén el parámetro q de la URL
+$q = $_REQUEST["q"];
+
+$hint = "";
+
+// busca todas las sugerencias del array si $q es diferente de ""
+if ($q !== "") {
+    $q = strtolower($q);
+    $len = strlen($q);
+    foreach($a as $name) {
+        if (stristr($q, substr($name, 0, $len))) {
+            if ($hint === "") {
+                $hint = $name;
+            } else {
+                $hint .= ", $name";
+            }
+        }
+    }
+}
+
+// Imprime la respuesta
+echo $hint === "" ? "no suggestion" : $hint;
+?>
